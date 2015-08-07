c73f7bcff28a9b3adf0da89074b7fce903265eae
plus12324 <sung-bm@hanmail.net>
2015-08-07 18:01:36 +0900

    d

---
+++ src/main/java/com/gSpring/controller/MainController.java
@@ -0,0 +1,23 @@
+package com.gSpring.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	@RequestMapping(value = "/home")
+	public void requestReAgree(final HttpServletRequest servletRequest, HttpServletResponse res,ModelMap mMap) {
+		
+		
+		System.out.println("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
+		
+		String CallbackMsg = "true";
+		
+		System.out.println("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
+		mMap.put("view", "home");
+	}
+}
