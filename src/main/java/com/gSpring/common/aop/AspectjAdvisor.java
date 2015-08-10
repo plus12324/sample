0933c1be0693102622854ce49b51dc0c3d082933
plus12324 <sung-bm@hanmail.net>
2015-08-10 17:42:05 +0900

    aop 적용

---
+++ src/main/java/com/gSpring/common/aop/AspectjAdvisor.java
@@ -0,0 +1,45 @@
+package com.gSpring.common.aop;
+
+import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.perf4j.LoggingStopWatch;
+import org.perf4j.StopWatch;
+
+@Aspect
+public class AspectjAdvisor {
+	private Logger logger = Logger.getLogger(getClass());
+	
+    @Pointcut("execution(public * com.gSpring.controller.MainController.*(..))" )
+    private void targetMethod(){}
+    
+    @Before("targetMethod()")
+    public void beforeTrace(JoinPoint jp){
+    	
+    }
+    
+    @Around("targetMethod()")
+ 	public Object trace(ProceedingJoinPoint joinPoint) throws Throwable {
+
+ 		String signatureString = joinPoint.getSignature().toShortString();
+
+ 		StopWatch sw = new LoggingStopWatch();
+ 		try {
+ 			logger.info("joinPoint.proceed()");
+ 			System.out.println("★★★★★★★★★★★★★joinPoint.proceed()_start");
+ 			Object result = joinPoint.proceed();
+ 			Thread.sleep(2000);
+ 			System.out.println("★★★★★★★★★★★★★joinPoint.proceed()_end");
+ 			return result;
+
+ 		} finally {
+ 			sw.stop("실행시간(ms)",signatureString);
+ 			sw.getMessage();
+ 		}
+ 	}
+    
+}
--- src/main/java/com/gSpring/controller/MainController.java
+++ src/main/java/com/gSpring/controller/MainController.java
@@ -3,21 +3,30 @@ package com.gSpring.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
+	
+	private Logger logger = Logger.getLogger(getClass());
+	
+	
 	@RequestMapping(value = "/home")
-	public String requestReAgree(final HttpServletRequest servletRequest, HttpServletResponse res,ModelMap mMap) {
+	public String home(final HttpServletRequest servletRequest, HttpServletResponse res,ModelMap mMap) {
 		
+		logger.info("메인화면 Controller");		
+				
+		return "home";
+	}
+	
+	@RequestMapping(value = "/home")
+	public void home(final HttpServletRequest servletRequest, HttpServletResponse res,ModelMap mMap) {
 		
-		System.out.println("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");		
-		String CallbackMsg = "true";		
-		System.out.println("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
-		
-		
+		logger.info("메인화면 Controller");		
+				
 		return "home";
 	}
 }
