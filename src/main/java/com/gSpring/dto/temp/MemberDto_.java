779a29f21c536db06e0fc2adf02a18face6e07e4
plus12324 <sung-bm@hanmail.net>
2015-08-12 17:35:21 +0900

    mybatis config

---
+++ src/main/java/com/gSpring/dto/temp/MemberDto_.java
@@ -0,0 +1,26 @@
+package com.gSpring.dto.temp;
+
+public class MemberDto_ {
+	private int USER_ID;
+	private String USER_NAME;
+	private String PASSWORD;
+	
+	public int getUSER_ID() {
+		return USER_ID;
+	}
+	public void setUSER_ID(int uSER_ID) {
+		USER_ID = uSER_ID;
+	}
+	public String getUSER_NAME() {
+		return USER_NAME;
+	}
+	public void setUSER_NAME(String uSER_NAME) {
+		USER_NAME = uSER_NAME;
+	}
+	public String getPASSWORD() {
+		return PASSWORD;
+	}
+	public void setPASSWORD(String pASSWORD) {
+		PASSWORD = pASSWORD;
+	}
+}
